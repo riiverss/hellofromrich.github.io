31309152d8a9e4023f845a143c74fb97243d21e9
riiverss <56989970+riiverss@users.noreply.github.com>
2021-06-27 16:37:02 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const sentences = [
 "Hello 🙋🏻‍♂️",
 "",
 "I'm Rich, a Software Engineering Recruiter here at Google and I am on the lookout to speak with Engineering folks for the teams right here in Sydney."],
-
+"",
+"We are looking for Engineers to join our Chrome, Photos & Maps teams."],
+      
 [
 "",
 "I wondered if you had given it any thought recently what your next career move may look like? 🤔",
