2f8a9ed4124024f2bb7f94a088719cbc5f9d491c
Rich <56989970+riiverss@users.noreply.github.com>
2022-02-10 13:47:58 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const sentences = [
-[
-"",
-"G'day!🏻",
-"",
-"My name is Rich, I'm a Software Engineering Recruiter here at Google in Sydney.",
-"",
-"I am currently researching for Software Engineers who would be interested in working on our Chrome, Photos or Maps Engineering teams."],
-
-[ 
-"",
-"I wondered if you had given it any thought recently what your next career move may look like?",
-"",
-"What sort of problems do you want to be solving, things you want to be building etc?"],
-[
-"",
-"I'm curious if you'd be open to a chat about what life could look like for you here at Google?",
-"",
-"How's my timing?",
-"",
-"👨🏻‍💻 Rich @Google.</a>"]];
+  [
+    "",
+    "G'day!",
+    "",
+    "My name is Rich, I'm a Software Engineering Recruiter here at Google in Sydney.",
+    "",
+    "I am currently researching for Software Engineers who could be interested in working on our Chrome, Photos or Maps Engineering teams."
+  ],
+  [ 
+    "",
+    "I wondered if you had given it any thought recently what your next career move may look like?",
+    "",
+    "What sort of problems do you want to be solving, things you want to be building etc?"
+  ],
+  [
+    "",
+    "I'm curious if you'd be open to a chat about what life could look like for you here at Google?",
+    "",
+    "How's my timing?",
+    "",
+    "👨🏻‍💻 Rich @Google.</a>"
+  ]
+];
 
 
 
